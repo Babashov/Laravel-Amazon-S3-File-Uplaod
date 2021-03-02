0c9758897d61614b0b587f23395df263b3b25392
Mahammad <babashov.info@gmail.com>
2021-03-02 23:05:24 +0400

    Folder Create and Show Folders Routes are ready

--- routes/web.php
+++ routes/web.php
@@ -12,3 +12,8 @@ Route::get('/images',[ImagesController::class,'create']);
 Route::post('/images',[ImagesController::class,"store"])->name('images');
 
 Route::get("/images/{image}",[ImagesController::class,"show"])->name('images.show');
+
+
+Route::get('/folder',[FolderController::class,"create"]);
+Route::post('/folder',[FolderController::class,"store"])->name("folder");
+Route::get("/folder/{folder}",[FolderController::class,["show"]);
