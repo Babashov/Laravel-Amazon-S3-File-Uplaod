395aeb7acd608f195770febf0efae91569dc0256
Mahammad <babashov.info@gmail.com>
2021-03-02 23:10:29 +0400

    FolderController Method Names Created

--- app/Http/Controllers/FolderController.php
+++ app/Http/Controllers/FolderController.php
@@ -3,8 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Folder;
 
 class FolderController extends Controller
 {
-    //
+    public function create()
+    {
+        return view("folder.create");
+    }
+
+    public function store(Request $request)
+    {
+        
+    }
+
+    public function show(Folder $folder)
+    {
+        
+    }
 }
