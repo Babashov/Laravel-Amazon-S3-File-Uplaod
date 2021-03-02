824213116e56f559d9d2d7e4eb3c15a5e94e5a5c
Mahammad <babashov.info@gmail.com>
2021-03-02 19:13:28 +0400

    Images Routes Added

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/images',[Images::class,['create']]);
+
+Route::post('/images',[Images::class,["strore"]]);
+
+Route::get("/images/{show}",[Images::class,["show"]]);
