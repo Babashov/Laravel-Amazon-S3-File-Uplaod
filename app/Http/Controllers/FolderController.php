2c11336bbde35c27b6bc9593f2f9067088ce6172
Mahammad <babashov.info@gmail.com>
2021-03-02 23:22:05 +0400

    Folder Create Post Method is testing

--- app/Http/Controllers/FolderController.php
+++ app/Http/Controllers/FolderController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Folder;
+use Illuminate\Support\Facades\Storage;
 
 class FolderController extends Controller
 {
@@ -14,11 +15,20 @@ class FolderController extends Controller
 
     public function store(Request $request)
     {
-        
+        $folder_name = $request->input("folder_name");
+        $folderCreate = Storage::disk('s3')->makeDirectory($folder_name);
+
+        dd($folderCreate);
+
+        // $folder = Folder::create([
+        //     "name"=>$folder_name,
+        //     "path"=>$folder_name
+        // ]);
+
     }
 
     public function show(Folder $folder)
     {
-        
+
     }
 }
